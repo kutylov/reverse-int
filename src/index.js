4a25585aa1de546527d2023e6af1b00123349c26
dolf <kolyakutylov@gmail.com>
2023-01-02 14:22:26 +0300

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let tempNumber = n < 0 ? -n : n
+
+  if (tempNumber % 10 === 0) {
+    tempNumber = tempNumber / 10
+  }
+
+  return +tempNumber.toString().split('').reverse().join('')
 }
